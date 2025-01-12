0c44a92fed5ed6c57a690c6f90a2eb8755a36024
sikzyo <183669693+Sikzyo@users.noreply.github.com>
2025-01-12 11:13:19 -0500

    Update primer tutorial terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ userName, name, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <article className="card">
+      <header className="card-header">
+        <img
+          className="card-img"
+          src={`https://unavatar.io/${userName}`}
+          alt="Foto de perfil"
+        />
+        <section className="card-info">
+          <strong>{name}</strong>
+          <span>@{userName}</span>
+        </section>
+      </header>
+      <aside className="card-actions">
+        <button className="card-button" onClick={handleClick}>
+          {text}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </aside>
+    </article>
+  );
 }
 
-export default App
+export default App;
